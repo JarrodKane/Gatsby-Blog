ec32652e94918daf14b8b77074399707c6ae342b
TrojinCat <kajarrod@gmail.com>
2019-07-26 15:39:14 +1000

    basic css added to site

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import headerStyles from  './header.module.scss'
 
 const Header = () => {
   return(
-    <header>
-    <h1>Blog</h1>
+    <header className={headerStyles.header}>
+    <h1><Link className={headerStyles.title} to="/">Jarrod Kane</Link></h1>
     <nav>
-    <ul>
-      <li>  <Link exact to="/">Home</Link></li>
-      <li>   <Link  to="/about">About</Link></li>
-      <li>    <Link  to="/blog">Blog</Link></li>
-      <li>    <Link  to="/contact">Contact</Link></li>
+    <ul className={headerStyles.navList}>
+      <li>  <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} exact to="/">Home</Link></li>
+      <li>   <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About</Link></li>
+      <li>    <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link></li>
+      <li>    <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">Contact</Link></li>
     </ul>
     </nav>
     
