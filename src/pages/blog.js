9ab041c60d40934f1758d271fa122513addd4dae
TrojinCat <kajarrod@gmail.com>
2019-07-28 15:00:17 +1000

    Added in links to the blog pages

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../components/layout'
-import {graphql, useStaticQuery } from 'gatsby'
+import {Link, graphql, useStaticQuery, } from 'gatsby'
 
 const BlogPage =() => {
   const data = useStaticQuery(graphql`
@@ -11,6 +11,9 @@ const BlogPage =() => {
           frontmatter {
             title
             date
+          }
+          fields {
+            slug
           }
         }
       }
@@ -27,10 +30,12 @@ const BlogPage =() => {
         {data.allMarkdownRemark.edges.map((edge) => {
           return (
             <li>
+            <Link to={`/blog/${edge.node.fields.slug}`}>
               <h2>
                   {edge.node.frontmatter.title}
               </h2>
               <p>{edge.node.frontmatter.date}</p>
+              </Link>
             </li>
           )
         })}
---
+++ src/templates/blog.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+
+const Blog = () => {
+
+  return(
+    <Layout>
+      this is a Blog template
+    </Layout>
+  )
+}
+
+export default Blog;
